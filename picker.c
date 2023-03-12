6d9accbb170393de526a082c8dfd4909f88b5b85
alpheratz0 <alpheratz99@protonmail.com>
2023-03-12 19:10:01 -0300

    picker: make hue selection not dynamic

--- picker.c
+++ picker.c
@@ -246,12 +246,9 @@ __picker_draw(Picker *picker)
 	}
 
 	for (dy = 0; dy < HUE_RECT_HEIGHT; dy++) {
-		col = __color_make_hsl(((float)(dy))/HUE_RECT_HEIGHT, picker->color.s, picker->color.l);
+		col = __color_make_hsl(((float)(dy))/HUE_RECT_HEIGHT, 0.5, 0.5);
 		for (dx = 0; dx < HUE_RECT_WIDTH; dx++) {
-			if (dx == 0 || dx == HUE_RECT_WIDTH-1 || dy == 0 || dy == HUE_RECT_HEIGHT-1)
-				picker->px[(PADDING+dy)*picker->width+HUE_RECT_X1+dx] = 0x777777;
-			else
-				picker->px[(PADDING+dy)*picker->width+HUE_RECT_X1+dx] = __color_to_uint32(col);
+			picker->px[(PADDING+dy)*picker->width+HUE_RECT_X1+dx] = __color_to_uint32(col);
 		}
 	}
 
