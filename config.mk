eda187f31283990d91cf760dabb3333bcf53c4fe
alpheratz0 <alpheratz99@protonmail.com>
2023-08-28 07:54:11 -0300

    format config.mk

--- config.mk
+++ config.mk
@@ -1,13 +1,11 @@
 # Copyright (C) 2023 <alpheratz99@protonmail.com>
 # This program is free software.
 
-VERSION   = 0.3.2
-
-CC        = cc
-INCS      = -I/usr/X11R6/include
-CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
-LDLIBS    = -lxcb -lxcb-shm -lxcb-image -lxcb-keysyms -lxcb-cursor -lm
-LDFLAGS   = -L/usr/X11R6/lib -s
-
-PREFIX    = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+VERSION=0.3.2
+CC=cc
+INCS=-I/usr/X11R6/include
+CFLAGS=-std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
+LDLIBS=-lxcb -lxcb-shm -lxcb-image -lxcb-keysyms -lxcb-cursor -lm
+LDFLAGS=-L/usr/X11R6/lib -s
+PREFIX=/usr/local
+MANPREFIX=$(PREFIX)/share/man
